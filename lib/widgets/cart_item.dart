097e27a248ec8cf99c59fb145a90fc59a0a8807a
Dipin Ahuja <dipinahuja99@gmail.com>
2021-06-29 16:22:03 +0530

    add edit_productscreen
    Forms Added

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -41,21 +41,25 @@ class CartItem extends StatelessWidget {
         return showDialog(
           context: context,
           builder: (ctx) => AlertDialog(
-            title: Text('Are You Sure'),
-            content: Text('Do you want to remove the item from the cart'),
-            actions: <Widget>[
-              FlatButton(
-                  onPressed: () {
-                    Navigator.of(ctx).pop(false);
-                  },
-                  child: Text('No')),
-              FlatButton(
-                  onPressed: () {
-                    Navigator.of(ctx).pop(true);
-                  },
-                  child: Text('Yes'))
-            ],
-          ),
+                title: Text('Are you sure?'),
+                content: Text(
+                  'Do you want to remove the item from the cart?',
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('No'),
+                    onPressed: () {
+                      Navigator.of(ctx).pop(false);
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('Yes'),
+                    onPressed: () {
+                      Navigator.of(ctx).pop(true);
+                    },
+                  ),
+                ],
+              ),
         );
       },
       onDismissed: (direction) {
